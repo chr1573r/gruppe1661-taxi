2302bbb149d8b0a2bccb72bdc3494621eba8af40
Christer Jonassen <christer.jonassen@gmail.com>
2013-10-17 12:15:47 +0200

    La til bilder i header.php

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <!-- Denne filen inneholder bare navigasjonsmenyen -->
 <ul>
+	<img src="img/PNG/Header.png" />
+	<img src="img/PNG/Menu.png" />
 <li><a href="index.php">Forside</a></li>
 <li><a href="taxi-informasjonside.php">Taxi informasjon</a></li>
 <li><a href="omoss.php">Om oss</a></li>
