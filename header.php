7710310f6744c6c4cecf1df0db212c713a95621e
Christer Jonassen <christer.jonassen@gmail.com>
2013-11-01 13:47:03 +0100

    Opprydding i CSS struktur
    IDer istedenfor generelle stiler

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!-- Denne filen inneholder bare navigasjonsmenyen -->
 	<img src="img/PNG/Header.png" />
 	<img src="img/PNG/Menu.png" />
-<ul id="menylinje">
+<ul id="meny_ul">
 <br>
-<li><a href="index.php">Forside</a></li>
-<li><a href="taxi-informasjonside.php">Taxi informasjon</a></li>
-<li><a href="omoss.php">Om oss</a></li>
-<li><a href="tilbakemelding.php">Tilbakemelding</a><li>
+<li id="meny_li"><a id="meny_a" href="index.php">Forside</a></li>
+<li id="meny_li"><a id="meny_a" href="taxi-informasjonside.php">Taxi informasjon</a></li>
+<li id="meny_li"><a id="meny_a" href="omoss.php">Om oss</a></li>
+<li id="meny_li"><a id="meny_a" href="tilbakemelding.php">Tilbakemelding</a><li>
 
 </ul>
