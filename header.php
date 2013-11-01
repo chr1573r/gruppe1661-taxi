649faacd690b2148005f2d6bc96f620cb33ce87b
Christer Jonassen <christer.jonassen@gmail.com>
2013-11-01 13:36:27 +0100

    Opprydning i header

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!-- Denne filen inneholder bare navigasjonsmenyen -->
-<ul>
 	<img src="img/PNG/Header.png" />
 	<img src="img/PNG/Menu.png" />
+<ul id="menylinje">
 <br>
 <li><a href="index.php">Forside</a></li>
 <li><a href="taxi-informasjonside.php">Taxi informasjon</a></li>
 <li><a href="omoss.php">Om oss</a></li>
-<li> <a href="tilbakemelding.php">Tilbakemelding</a><li>
+<li><a href="tilbakemelding.php">Tilbakemelding</a><li>
 
 </ul>
