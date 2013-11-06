cc95405ad0f4e9b603194013318fd08dfc509945
Kevin <kevin.chisholm.hansen@gmail.com>
2013-11-06 11:01:15 +0100

    Update tilbakemelding.php

--- tilbakemelding.php
+++ tilbakemelding.php
@@ -1,50 +1,107 @@
-<html dir="ltr" lang="en-US">
-<head>
-  <meta charset="UTF-8" />
-  <title>TaxiCalc - Tilbakemelding</title>
-  <link rel="stylesheet" type="text/css" href="taxicalc.css">
-</head>
-<body>
-<div id="fb-root"></div>
-<script>
-  window.fbAsyncInit = function() {
-   
-    FB.init({
-      appId      : '170352443159090',                       
-      channelUrl : 'http://prosjekt.hia.no/users/chrisj13/prosjekt/taxicalc/tilbakemelding.html', 
-      status     : true,                                 
-                                      
-    });
+<?php
+/**
+ * Copyright 2011 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 
-   
-  };
+require '../src/facebook.php';
 
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "http://prosjekt.hia.no/users/chrisj13/prosjekt/taxicalc/tilbakemelding.html";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-	
-</script>
+// Create our Application instance (replace this with your appId and secret).
+$facebook = new Facebook(array(
+  'appId'  => '170352443159090',
+  'secret' => '234a09d37fb5d1131aadf8f8155a528b',
+));
 
-<head>
+// Get User ID
+$user = $facebook->getUser();
 
-<meta charset="UTF-8" />
-<title>Tilbakemelding </title>
-		
-<?php include("header.php");?>
+// We may or may not have this data based on whether the user is logged in.
+//
+// If we have a $user id here, it means we know the user is logged into
+// Facebook, but we don't know if the access token is valid. An access
+// token is invalid if the user logged out of Facebook.
 
-<div class="fb-comments"  
-     data-colorscheme="light"	
-     data-href="http://prosjekt.hia.no/users/chrisj13/prosjekt/taxicalc/tilbakemelding.html"
-     data-num-posts="6"
-     data-width="550">
-</div>
+if ($user) {
+  try {
+    // Proceed knowing you have a logged in user who's authenticated.
+    $user_profile = $facebook->api('/me');
+  } catch (FacebookApiException $e) {
+    error_log($e);
+    $user = null;
+  }
+}
 
-<br>
- 
-</body>
-<?php include("footer.php");?>
+// Login or logout url will be needed depending on current user state.
+if ($user) {
+  $logoutUrl = $facebook->getLogoutUrl();
+} else {
+  $statusUrl = $facebook->getLoginStatusUrl();
+  $loginUrl = $facebook->getLoginUrl();
+}
+
+// This call will always work since we are fetching public data.
+$naitik = $facebook->api('/naitik');
+
+?>
+<!doctype html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+  <head>
+    <title>php-sdk</title>
+    <style>
+      body {
+        font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
+      }
+      h1 a {
+        text-decoration: none;
+        color: #3b5998;
+      }
+      h1 a:hover {
+        text-decoration: underline;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>php-sdk</h1>
+
+    <?php if ($user): ?>
+      <a href="<?php echo $logoutUrl; ?>">Logout</a>
+    <?php else: ?>
+      <div>
+        Check the login status using OAuth 2.0 handled by the PHP SDK:
+        <a href="<?php echo $statusUrl; ?>">Check the login status</a>
+      </div>
+      <div>
+        Login using OAuth 2.0 handled by the PHP SDK:
+        <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
+      </div>
+    <?php endif ?>
+
+    <h3>PHP Session</h3>
+    <pre><?php print_r($_SESSION); ?></pre>
+
+    <?php if ($user): ?>
+      <h3>You</h3>
+      <img src="https://graph.facebook.com/<?php echo $user; ?>/picture">
+
+      <h3>Your User Object (/me)</h3>
+      <pre><?php print_r($user_profile); ?></pre>
+    <?php else: ?>
+      <strong><em>You are not Connected.</em></strong>
+    <?php endif ?>
+
+    <h3>Public profile of Naitik</h3>
+    <img src="https://graph.facebook.com/naitik/picture">
+    <?php echo $naitik['name']; ?>
+  </body>
 </html>
