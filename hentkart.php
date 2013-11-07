08f3ea99774f3b05f89c1d0a1534895fde675672
Christer Jonassen <christer.jonassen@gmail.com>
2013-11-07 13:43:33 +0100

    Oppdatert med nye php tagger

--- hentkart.php
+++ hentkart.php
@@ -4,123 +4,183 @@
 switch ("$rutekode") {
 
 case "uikuig":
+?>
 // SETT INN KART FOR uik-uig PÅ DENNE LINJEN
+<?php
 break;
 
 case "uikkrs":
-// SETT INN KART FOR uik-krs PÅ DENNE LINJEN
+?>
+<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=d&amp;source=s_d&amp;saddr=University+of+Agder,+Kristiansand,+Norway&amp;daddr=58.1581955,8.0052379+to:Markens+gate,+Kristiansand,+Norway&amp;hl=en&amp;geocode=FXqBdwMdfh96ACFS4kYnqS4Y7ymTrJWFUwI4RjFS4kYnqS4Y7w%3BFXNsdwMddSZ6AClLDNqrUQI4RjGV4CAlnO3JlA%3BFeg3dwMde_15ACn1skoXXgI4RjEsgAcRW5R5yA&amp;aq=0&amp;oq=markens&amp;sll=58.269788,8.285751&amp;sspn=0.20331,0.607681&amp;t=m&amp;mra=ls&amp;ie=UTF8&amp;via=1&amp;ll=58.15444,8.003386&amp;spn=0.019392,0.031796&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=d&amp;source=embed&amp;saddr=University+of+Agder,+Kristiansand,+Norway&amp;daddr=58.1581955,8.0052379+to:Markens+gate,+Kristiansand,+Norway&amp;hl=en&amp;geocode=FXqBdwMdfh96ACFS4kYnqS4Y7ymTrJWFUwI4RjFS4kYnqS4Y7w%3BFXNsdwMddSZ6AClLDNqrUQI4RjGV4CAlnO3JlA%3BFeg3dwMde_15ACn1skoXXgI4RjEsgAcRW5R5yA&amp;aq=0&amp;oq=markens&amp;sll=58.269788,8.285751&amp;sspn=0.20331,0.607681&amp;t=m&amp;mra=ls&amp;ie=UTF8&amp;via=1&amp;ll=58.15444,8.003386&amp;spn=0.019392,0.031796" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+<?php
 break;
 
 case "uikman":
+?>
 // SETT INN KART FOR uik-man PÅ DENNE LINJEN
+<?php
 break;
 
 case "uikven":
+?>
 // SETT INN KART FOR uik-ven PÅ DENNE LINJEN
+<?php
 break;
 
 case "uiksog":
+?>
 // SETT INN KART FOR uik-sog PÅ DENNE LINJEN
+<?php
 break;
 
 case "uiguik":
+?>
 // SETT INN KART FOR uig-uik PÅ DENNE LINJEN
+<?php
 break;
 
 case "uigkrs":
+?>
 // SETT INN KART FOR uig-krs PÅ DENNE LINJEN
+<?php
 break;
 
 case "uigman":
+?>
 // SETT INN KART FOR uig-man PÅ DENNE LINJEN
+<?php
 break;
 
 case "uigven":
+?>
 // SETT INN KART FOR uig-ven PÅ DENNE LINJEN
+<?php
 break;
 
 case "uigsog":
+?>
 // SETT INN KART FOR uig-sog PÅ DENNE LINJEN
+<?php
 break;
 
 case "krsuik":
+?>
 // SETT INN KART FOR krs-uik PÅ DENNE LINJEN
+<?php
 break;
 
 case "krsuig":
+?>
 // SETT INN KART FOR krs-uig PÅ DENNE LINJEN
+<?php
 break;
 
 case "krsman":
+?>
 // SETT INN KART FOR krs-man PÅ DENNE LINJEN
+<?php
 break;
 
 case "krsven":
+?>
 // SETT INN KART FOR krs-ven PÅ DENNE LINJEN
+<?php
 break;
 
 case "krssog":
+?>
 // SETT INN KART FOR krs-sog PÅ DENNE LINJEN
+<?php
 break;
 
 case "manuik":
+?>
 // SETT INN KART FOR man-uik PÅ DENNE LINJEN
+<?php
 break;
 
 case "manuig":
+?>
 // SETT INN KART FOR man-uig PÅ DENNE LINJEN
+<?php
 break;
 
 case "mankrs":
+?>
 // SETT INN KART FOR man-krs PÅ DENNE LINJEN
+<?php
 break;
 
 case "manven":
+?>
 // SETT INN KART FOR man-ven PÅ DENNE LINJEN
+<?php
 break;
 
 case "mansog":
+?>
 // SETT INN KART FOR man-sog PÅ DENNE LINJEN
+<?php
 break;
 
 case "venuik":
+?>
 // SETT INN KART FOR ven-uik PÅ DENNE LINJEN
+<?php
 break;
 
 case "venuig":
+?>
 // SETT INN KART FOR ven-uig PÅ DENNE LINJEN
+<?php
 break;
 
 case "venkrs":
+?>
 // SETT INN KART FOR ven-krs PÅ DENNE LINJEN
+<?php
 break;
 
 case "venman":
+?>
 // SETT INN KART FOR ven-man PÅ DENNE LINJEN
+<?php
 break;
 
 case "vensog":
+?>
 // SETT INN KART FOR ven-sog PÅ DENNE LINJEN
+<?php
 break;
 
 case "soguik":
+?>
 // SETT INN KART FOR sog-uik PÅ DENNE LINJEN
+<?php
 break;
 
 case "soguig":
+?>
 // SETT INN KART FOR sog-uig PÅ DENNE LINJEN
+<?php
 break;
 
 case "sogkrs":
+?>
 // SETT INN KART FOR sog-krs PÅ DENNE LINJEN
+<?php
 break;
 
 case "sogman":
+?>
 // SETT INN KART FOR sog-man PÅ DENNE LINJEN
+<?php
 break;
 
 case "sogven":
+?>
 // SETT INN KART FOR sog-ven PÅ DENNE LINJEN
+<?php
 break;
 
 case "feil":
