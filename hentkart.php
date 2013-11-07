288026ba2785b784e5b979470bafca056c502265
Christer Jonassen <christer.jonassen@gmail.com>
2013-11-07 12:44:37 +0100

    hentkart.php
    Her skal html kode for kart legges inn senere

---
+++ hentkart.php
@@ -0,0 +1,130 @@
+<?php
+
+// Case statements laget automatisk med lag_ruter.sh
+switch ("$rutekode") {
+
+case "uikuig":
+// SETT INN KART FOR uik-uig PÅ DENNE LINJEN
+break;
+
+case "uikkrs":
+// SETT INN KART FOR uik-krs PÅ DENNE LINJEN
+break;
+
+case "uikman":
+// SETT INN KART FOR uik-man PÅ DENNE LINJEN
+break;
+
+case "uikven":
+// SETT INN KART FOR uik-ven PÅ DENNE LINJEN
+break;
+
+case "uiksog":
+// SETT INN KART FOR uik-sog PÅ DENNE LINJEN
+break;
+
+case "uiguik":
+// SETT INN KART FOR uig-uik PÅ DENNE LINJEN
+break;
+
+case "uigkrs":
+// SETT INN KART FOR uig-krs PÅ DENNE LINJEN
+break;
+
+case "uigman":
+// SETT INN KART FOR uig-man PÅ DENNE LINJEN
+break;
+
+case "uigven":
+// SETT INN KART FOR uig-ven PÅ DENNE LINJEN
+break;
+
+case "uigsog":
+// SETT INN KART FOR uig-sog PÅ DENNE LINJEN
+break;
+
+case "krsuik":
+// SETT INN KART FOR krs-uik PÅ DENNE LINJEN
+break;
+
+case "krsuig":
+// SETT INN KART FOR krs-uig PÅ DENNE LINJEN
+break;
+
+case "krsman":
+// SETT INN KART FOR krs-man PÅ DENNE LINJEN
+break;
+
+case "krsven":
+// SETT INN KART FOR krs-ven PÅ DENNE LINJEN
+break;
+
+case "krssog":
+// SETT INN KART FOR krs-sog PÅ DENNE LINJEN
+break;
+
+case "manuik":
+// SETT INN KART FOR man-uik PÅ DENNE LINJEN
+break;
+
+case "manuig":
+// SETT INN KART FOR man-uig PÅ DENNE LINJEN
+break;
+
+case "mankrs":
+// SETT INN KART FOR man-krs PÅ DENNE LINJEN
+break;
+
+case "manven":
+// SETT INN KART FOR man-ven PÅ DENNE LINJEN
+break;
+
+case "mansog":
+// SETT INN KART FOR man-sog PÅ DENNE LINJEN
+break;
+
+case "venuik":
+// SETT INN KART FOR ven-uik PÅ DENNE LINJEN
+break;
+
+case "venuig":
+// SETT INN KART FOR ven-uig PÅ DENNE LINJEN
+break;
+
+case "venkrs":
+// SETT INN KART FOR ven-krs PÅ DENNE LINJEN
+break;
+
+case "venman":
+// SETT INN KART FOR ven-man PÅ DENNE LINJEN
+break;
+
+case "vensog":
+// SETT INN KART FOR ven-sog PÅ DENNE LINJEN
+break;
+
+case "soguik":
+// SETT INN KART FOR sog-uik PÅ DENNE LINJEN
+break;
+
+case "soguig":
+// SETT INN KART FOR sog-uig PÅ DENNE LINJEN
+break;
+
+case "sogkrs":
+// SETT INN KART FOR sog-krs PÅ DENNE LINJEN
+break;
+
+case "sogman":
+// SETT INN KART FOR sog-man PÅ DENNE LINJEN
+break;
+
+case "sogven":
+// SETT INN KART FOR sog-ven PÅ DENNE LINJEN
+break;
+
+case "feil":
+break;
+
+}
+?>
