9a6b65f586763887d1aa07f603809fbdb207ab2b
Paalsandnes91 <sandnes@pal-mbp-2.lan>
2013-11-07 19:20:20 +0100

    Fjernet lik oss på tilbakemelding.

--- tilbakemelding.php
+++ tilbakemelding.php
@@ -25,24 +25,6 @@
 
 
 <div class="fb-comments" data-href="https://www.facebook.com/pages/Taxicalc/325718240901463" data-colorscheme="dark" data-numposts="10" data-width="800"></div>
-</body>
-
-<body>
-<h1> Lik oss! </h1>
-
-	<div align="left">
-
-
-	<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/nb_NO/all.js#xfbml=1&appId=1390598977846946";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<div class="fb-like" data-href="https://www.facebook.com/pages/Taxicalc/325718240901463" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
 
 </body>
 
