8469b5a0f5e4bf50a3ecd748c31c897f8c57536b
Christer Jonassen <christer.jonassen@gmail.com>
2013-11-07 12:43:42 +0100

    Oppdatert til å generere php kode

--- verktoy/lag_ruter/lag_ruter.sh
+++ verktoy/lag_ruter/lag_ruter.sh
@@ -4,13 +4,28 @@
 
 # Lager automatisk unike ruteverdier. f.eks krsman, sogven
 # lager A-B og B-A av steder lagt til i filen steder.txt
+# Genererer disse inn i switch statements for php
 
+echo -e "<?php"
+echo -e ""
+echo -e "// Case statements laget automatisk med lag_ruter.sh"
+echo -e "switch (\"\$rutekode\") {"
+echo -e ""
 while read -r fra
 do
+	
 	while read -r til
 	do
 		if [ "$fra" != "$til" ]; then
-		echo "$fra$til"
+    		echo -e "case \"$fra$til\":"
+    		echo -e "// SETT INN KART FOR $fra-$til PÅ DENNE LINJEN"
+    		echo -e "break;"
+    		echo -e ""
 		fi
 	done < steder.txt
 done < steder.txt
+ echo -e "case \"feil\":"
+ echo -e "break;"
+ echo -e ""
+echo -e "}"
+echo -e "?>"
