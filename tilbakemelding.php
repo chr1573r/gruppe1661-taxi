288f6def164720986a6966c62943cc6b4913307c
Paalsandnes91 <sandnes@krs-eduroam-s-158-37-242-175.uia.no>
2013-11-07 14:25:23 +0100

    Ny endring tilbakemelding. php

--- tilbakemelding.php
+++ tilbakemelding.php
@@ -2,14 +2,17 @@
 <head>
 </head>
 <body>
+
+
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "https://connect.facebook.net/nb_NO/all.js#xfbml=1&appId=1390598977846946";
+  js.src = "//connect.facebook.net/nb_NO/all.js#xfbml=1&appId=1390598977846946";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+
 
 <div class="fb-comments" data-href="https://www.facebook.com/pages/Taxicalc/325718240901463" data-colorscheme="light" data-numposts="10" data-width="800"></div>
 </body>
