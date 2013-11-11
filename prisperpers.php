4747b0c76077f432daa24a2ec76674dfcfec032d
Paalsandnes91 <sandnes@krs-eduroam-s-158-37-248-69.uia.no>
2013-11-11 13:02:45 +0100

    Skyt meg

--- prisperpers.php
+++ prisperpers.php
@@ -5,8 +5,8 @@ $pris=$_POST['pris_taxi']; //Henter antall km fra form
 $antall= $_POST['antall_person'];
 $produkt= $pris/$antall;
 {
-$produkt= ($pris/$antall);
+echo "$produkt= ($pris/$antall)";
 echo "pris_pr.person";
 
-include ("index.php");
+include("index.php");
 ?>
