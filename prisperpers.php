8d612718bc5210992211c2815ec0207764c2995d
Paalsandnes91 <sandnes@krs-eduroam-s-158-37-248-69.uia.no>
2013-11-11 12:52:05 +0100

    glemte ";"

--- prisperpers.php
+++ prisperpers.php
@@ -2,10 +2,10 @@
 <?php
 $pris=$_POST['pris_taxi']; //Henter antall km fra form
 
-$antall= $_POST['antall_person']
-$produkt=  $pris/$antall;
-{
+$antall= $_POST['antall_person'];
 $produkt= $pris/$antall;
+{
+$produkt= ($pris/$antall);
 echo "pris_pr.person";
 
 include 'index.php';
