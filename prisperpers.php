2c3fb0d095dbfe1dde812d8935362983bdec950e
nadian13 <nadia.nabiyar13@gmail.com>
2013-11-13 11:37:30 +0100

    test php

--- prisperpers.php
+++ prisperpers.php
@@ -1,12 +1,12 @@
 
 <?php
 $pris=$_POST['pris_taxi']; 
-$kode =$_POST['kode']
+$kode =$_POST['kode'];
 $produkt= ($pris/$kode);
 
 if ( $kode == "2" )
 {
-echo "$produkt= ($pris/$kode)";
+echo $produkt= ($pris/$kode);
 echo "pris_pr.person";
 }
 if ( $kode == "3" )
