e3af44f03b78e5116cfc134e72de18ce7e4daa85
nadian13 <nadia.nabiyar13@gmail.com>
2013-11-13 11:22:00 +0100

    php

--- prisperpers.php
+++ prisperpers.php
@@ -6,22 +6,22 @@ $produkt= $pris/$kode;
 
 if ( $kode == "2" )
 {
-echo "$produkt= ($pris/2)";
+echo "$produkt= ($pris/$kode)";
 echo "pris_pr.person";
 }
 if ( $kode == "3" )
 {
-echo "$produkt= ($pris/3)";
+echo "$produkt= ($pris/$kode)";
 echo "pris_pr.person";
 }
 if ( $kode == "4" )
 {
-echo "$produkt= ($pris/4)";
+echo "$produkt= ($pris/$kode)";
 echo "pris_pr.person";
 }
 if ( $kode == "5" )
 {
-echo "$produkt= ($pris/5)";
+echo "$produkt= ($pris/$kode)";
 echo "pris_pr.person";
 }
 
