84729f8bf980e99589965148bb5e004185ea1158
Christer Jonassen <christer.jonassen@gmail.com>
2013-11-14 12:32:44 +0100

    Fjernet et linjeskift

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 	<img src="img/PNG/Header.png" />
 	<!--<img src="img/PNG/Menu.png" />-->
 <ul align="center" id="meny_ul">
-<br>
 <li id="meny_li"><a id="meny_a" href="index.php">Forside</a></li>
 <li id="meny_li"><a id="meny_a" href="taxi-informasjonside.php">Taxi informasjon</a></li>
 <li id="meny_li"><a id="meny_a" href="omoss.php">Om oss</a></li>
