a6be2f32d569915d92fc9e3ea6b131ec7af1081a
Paalsandnes91 <sandnes@krs-eduroam-s-158-37-240-62.uia.no>
2013-11-14 11:09:09 +0100

    endret adresse

--- tilbakemelding.php
+++ tilbakemelding.php
@@ -30,7 +30,7 @@
 <br>
 <br>
 
-<script type="text/javascript" src="http://www.yr.no/sted/Norge/vest_agder/Kristiansand/ekstern_boks_liten.js"></script><noscript><a href="http://www.yr.no/sted/Norge/Møre_og_Romsdal/Kristiansund/Kristiansund_lufthavn,_Kvernberget/">yr.no: Værvarsel for Kristiansund lufthavn, Kvernberget</a></noscript>
+<script type="text/javascript" src="http://www.yr.no/sted/Norge/vest_agder/Kristiansand/ekstern_boks_liten.js"></script><noscript><a href="http://www.yr.no/sted/Norge/Møre_og_Romsdal/Kristiansund/Kristiansund_lufthavn,_Kvernberget/">yr.no: Værvarsel for Kristiansand</a></noscript>
 
 </body>
 
