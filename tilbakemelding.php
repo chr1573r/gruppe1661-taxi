e2d63e111632c4d41e8d671ea81821338ef0532c
Paalsandnes91 <sandnes@krs-eduroam-s-158-37-240-62.uia.no>
2013-11-14 11:32:30 +0100

    tilbakemelding

--- tilbakemelding.php
+++ tilbakemelding.php
@@ -23,10 +23,20 @@
   js.src = "//connect.facebook.net/nb_NO/all.js#xfbml=1&appId=1390598977846946";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/nb_NO/all.js#xfbml=1&appId=1390598977846946";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
 
 <div class="fb-like-box" data-href="https://www.facebook.com/pages/Taxicalc/325718240901463" data-width="600" data-colorscheme="dark" data-show-faces="true" data-header="true" data-stream="true" data-show-border="true"></div>
+<div class="fb-comments" data-href="http://www.facebook.com/pages/Taxicalc/325718240901463" data-colorscheme="dark" data-numposts="20" data-width="600"></div>
 <br>
+
 <br>
 
 <div align="left">
