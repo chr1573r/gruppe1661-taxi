ba7cad77c800834edbfe02d5e20dea0e1d7795dd
Paalsandnes91 <sandnes@krs-eduroam-s-158-37-240-62.uia.no>
2013-11-14 10:59:40 +0100

    yr?

--- tilbakemelding.php
+++ tilbakemelding.php
@@ -29,6 +29,8 @@
 <div class="fb-comments" data-href="https://www.facebook.com/pages/Taxicalc/325718240901463" data-colorscheme="dark" data-numposts="20" data-width="600"></div>
 
 
+Blindern Byområde Norge 2013-11-14T04:42:22 2013-11-14T11:00:00 torsdag <strong>Østlandet:</strong> Dreining til sørlig bris, liten kuling på kysten, minkende til kvelden. Litt sludd eller snø av og til, for det meste regn i lavereliggende områder sør for ca. Hamar. Først på dagen stedvis utrygt for regn som fryser på bakken sør for Lillehammer. fredag <strong>Østlandet:</strong> Vestlig bris. For det meste skyet. Opphold. Fra fredag formiddag sørvestlig bris, frisk bris på kysten. Om kvelden til dels stiv kuling i ytre Oslofjord. Etter hvert litt regn, vesentlig nord for Lillehammer, snø i høyden. lørdag <strong>Østlandet:</strong> Sørvestlig bris, frisk bris på kysten. Om kvelden vestlig bris. Stort sett oppholdsvær og perioder med sol. Mildt. søndag og mandag <strong>Sør-Norge:</strong> Vind mellom vest og sør, av og til kuling på kysten og i fjellet. Lite eller ingen nedbør Østafjells. Ellers perioder med regn og regnbyger, sludd- eller snø i indre og høyere strøk. Lavere temperatur. tirsdag til fredag <strong>Norge og Spitsbergen:</strong> Fortsatt lavtrykksaktivitet i Norskehavet og Nordsjøen gir urolig vær i Skandinavia. Det ventes vind for det meste mellom sørvest og nordvest, perioder med kuling på kysten. Bygevær, vesentlig som snø i nord. Lite nedbør i indre strøk. Litt synkende temperatur. På Spitsbergen overveiende nordlig vind, av og til kuling. Litt snø av og til. Litt kaldere.
+
 </body>
 
 <?php include("footer.php");?>
