82482dd9449e06bbddd4e8389c9c288d5f393eb4
nadian13 <nadia.nabiyar13@gmail.com>
2013-11-25 11:49:46 +0100

    logoen
    fjerner svarte billede fra logoen

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <!-- Denne filen inneholder bare navigasjonsmenyen -->
 <div id="innhold">
 	<img src="img/PNG/Header.png" />
-	<img src="img/PNG/Menu.png" />
 <ul align="center" id="meny_ul">
 <li id="meny_li"><a id="meny_a" href="index.php">Forside</a></li>
 <li id="meny_li"><a id="meny_a" href="taxi-informasjonside.php">Taxi informasjon</a></li>
