0e066dbd3ec73df7475601665e389e76b993d665
Christer Jonassen <christer.jonassen@gmail.com>
2013-11-25 13:25:07 +0100

    Bedret lesbarhet, fikset id henvisning som skulle vært class

--- header.php
+++ header.php
@@ -1,61 +1,62 @@
 <!-- Denne filen inneholder bare navigasjonsmenyen -->
 <div id="innhold">
 	<img src="img/PNG/Header.png" />
-<ul align="center" id="meny_ul">
+<ul align="center" class="meny_ul">
 <?php
 $side="forside";
 if ($side="forside")
-{
-?>
-<li class="meny_li"><a class="meny_a" id="markert" href="index.php">Forside</a></li>
-<?
-}
+	{ //Hvis side er forside:
+		?>
+		<li class="meny_li"><a class="meny_a" id="markert" href="index.php">Forside</a></li>
+		<?
+	}
 else
-{
-?>
-<li class="meny_li"><a class="meny_a" href="index.php">Forside</a></li>
-<?
-}
+	{
+		?>
+		<li class="meny_li"><a class="meny_a" href="index.php">Forside</a></li>
+		<?
+	}
 if ($side="taxiinformasjon")
-{
-?>
-<li class="meny_li"><a class="meny_a" id="markert" href="taxi-informasjonside.php">Taxi informasjon</a></li>
-<?
-}
+	{ //Hvis side er taxiinformasjon:
+		?>
+		<li class="meny_li"><a class="meny_a" id="markert" href="taxi-informasjonside.php">Taxi informasjon</a></li>
+		<?
+	}
 
 else
-{
-?>
-<li class="meny_li"><a class="meny_a" href="taxi-informasjonside.php">Taxi informasjon</a></li>
-<?
-}
+	{
+		?>
+		<li class="meny_li"><a class="meny_a" href="taxi-informasjonside.php">Taxi informasjon</a></li>
+		<?
+	}
 
 if ($side="omoss")
-{
-?>
-<li class="meny_li"><a class="meny_a" id="markert" href="omoss.php">Om oss</a></li>
-<?
-}
+	{ //Hvis side er om oss:
+		?>
+		<li class="meny_li"><a class="meny_a" id="markert" href="omoss.php">Om oss</a></li>
+		<?
+	}
 
 else
-{
-?>
-<li class="meny_li"><a class="meny_a" href="omoss.php">Om oss</a></li>
-<?
-}
+	{
+		?>
+		<li class="meny_li"><a class="meny_a" href="omoss.php">Om oss</a></li>
+		<?
+	}
+
+
 if ($side="tilbakemelding")
-{
-?>
-<li class="meny_li"><a class="meny_a" id="markert" href="tilbakemelding.php">Tilbakemelding</a><li>
-<?
-}
-
+	{ //Hvis side er tilbakemelding:
+		?> 
+		<li class="meny_li"><a class="meny_a" id="markert" href="tilbakemelding.php">Tilbakemelding</a><li>
+		<?
+	}
 else
-{
-?>
-<li class="meny_li"><a class="meny_a" href="tilbakemelding.php">Tilbakemelding</a><li>
-<?
-} 
+	{
+		?>
+		<li class="meny_li"><a class="meny_a" href="tilbakemelding.php">Tilbakemelding</a><li>
+		<?
+	} 
 ?>
 
 </ul> 
