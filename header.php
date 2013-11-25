3fe19ec551470fb314ebf6a97aeb0d9db99696d8
nadian13 <nadia.nabiyar13@gmail.com>
2013-11-25 12:36:21 +0100

    Endret CSS struktur for meny

--- header.php
+++ header.php
@@ -4,32 +4,32 @@
 <ul align="center" id="meny_ul">
 if side=Forside 
 {
-<li id="meny_li"><a id="meny_a" class="markert" href="index.php">Forside</a></li>
+<li class="meny_li"><a class="meny_a" id="markert" href="index.php">Forside</a></li>
 else
 
-<li id="meny_li"><a id="meny_a" href="index.php">Forside</a></li>
+<li class="meny_li"><a class="meny_a" href="index.php">Forside</a></li>
 }
 if side= Taxi informasjon
 {
-<li id="meny_li"><a id="meny_a" class="markert" href="taxi-informasjonside.php">Taxi informasjon</a></li>
+<li class="meny_li"><a class="meny_a" id="markert" href="taxi-informasjonside.php">Taxi informasjon</a></li>
 else
 
-<li id="meny_li"><a id="meny_a" href="taxi-informasjonside.php">Taxi informasjon</a></li>
+<li class="meny_li"><a class="meny_a" href="taxi-informasjonside.php">Taxi informasjon</a></li>
 }
 
 if side= Om oss
 {
-<li id="meny_li"><a id="meny_a" class="markert" href="omoss.php">Om oss</a></li>
+<li class="meny_li"><a class="meny_a" id="markert" href="omoss.php">Om oss</a></li>
 else
 
-<li id="meny_li"><a id="meny_a" href="omoss.php">Om oss</a></li>
+<li class="meny_li"><a class="meny_a" href="omoss.php">Om oss</a></li>
 }
 if side= Tilbakemelding
 {
-<li id="meny_li"><a id="meny_a" class="markert" href="tilbakemelding.php">Tilbakemelding</a><li>
+<li class="meny_li"><a class="meny_a" id="markert" href="tilbakemelding.php">Tilbakemelding</a><li>
 else
 
-<li id="meny_li"><a id="meny_a" href="tilbakemelding.php">Tilbakemelding</a><li>
+<li class="meny_li"><a class="meny_a" href="tilbakemelding.php">Tilbakemelding</a><li>
 } 
 <?
 
