700f511b183061a67d1a1faac69de7b98e7eee49
Paalsandnes91 <sandnes@krs-eduroam-s-158-37-250-212.uia.no>
2013-11-25 12:16:25 +0100

    Endret skrivefeil.. "Omoss.php"

--- omoss.php
+++ omoss.php
@@ -18,35 +18,35 @@
 Fra Danmark <br>
 Funfact:Nei... <br> 
 Hobby:<br>
-jobberfaringer:<br><br>
+Jobberfaringer:<br><br>
 
 <h3> Christer Jonassen: </h3>
 23 år <br>
 Fra Stavanger <br>
 Funfact: Var gitarist i et "screamo"/metal band<br> 
 Hobby:<br>
-jobberfaringer:<br><br>
+Jobberfaringer:<br><br>
 
 <h3> Christoffer Lien </h3>
 24 år <br>
 Fra Kristiansand <br>
 Funfact: Tror på spøkelser <br> 
 Hobby:<br>
-jobberfaringer:<br><br>
+Jobberfaringer:<br><br>
 
 <h3> Kevin Chisholm </h3>
 23 år <br>
 Fra Bergen <br>
 Funfact: Har en morbid humor <br> 
 Hobby:<br>
-jobberfaringer:<br><br>
+Jobberfaringer:<br><br>
 
 <h3> Pål Sandnes </h3>
 21 år <br>
 Fra Risør <br>
 Funfact: Begynner å gråte hvis han taper 3 FIFA-kamper på rad
 Hobby:<br>
-jobberfaringer:<br><br>
+Jobberfaringer:<br><br>
 
 
 
