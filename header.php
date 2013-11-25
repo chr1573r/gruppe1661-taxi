250d534b5797ba360fc4a30c969be2fcda2f86b2
nadian13 <nadia.nabiyar13@gmail.com>
2013-11-25 13:03:52 +0100

    test

--- header.php
+++ header.php
@@ -6,37 +6,54 @@
 $side="forside";
 if ($side="forside")
 {
+?>
 <li class="meny_li"><a class="meny_a" id="markert" href="index.php">Forside</a></li>
+<?
 else
-
+{
+?>
 <li class="meny_li"><a class="meny_a" href="index.php">Forside</a></li>
+<?
 }
 if ($side="taxiinformasjon")
 {
+?>
 <li class="meny_li"><a class="meny_a" id="markert" href="taxi-informasjonside.php">Taxi informasjon</a></li>
+<?
 }
+
 else
 {
+?>
 <li class="meny_li"><a class="meny_a" href="taxi-informasjonside.php">Taxi informasjon</a></li>
+<?
 }
 
 if ($side="omoss")
 {
+?>
 <li class="meny_li"><a class="meny_a" id="markert" href="omoss.php">Om oss</a></li>
+<?
 }
-{
-else
 
+else
+{
+?>
 <li class="meny_li"><a class="meny_a" href="omoss.php">Om oss</a></li>
+<?
 }
 if ($side="tilbakemelding")
 {
+?>
 <li class="meny_li"><a class="meny_a" id="markert" href="tilbakemelding.php">Tilbakemelding</a><li>
+<?
 }
-{
-else
 
+else
+{
+?>
 <li class="meny_li"><a class="meny_a" href="tilbakemelding.php">Tilbakemelding</a><li>
+<?
 } 
 ?>
 
