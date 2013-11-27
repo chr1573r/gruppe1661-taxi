9e955c5756f16d392edd850965be72c2dd4edc90
Paalsandnes91 <sandnes@Pal-MacBook-Pro-2.local>
2013-11-27 12:40:45 +0100

    tester

--- tilbakemelding.php
+++ tilbakemelding.php
@@ -26,21 +26,10 @@ include("header.php");?>
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/nb_NO/all.js#xfbml=1&appId=1390598977846946";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<br>
 
 <div align="left">
 <div class="fb-comments" data-href="http://www.facebook.com/pages/Taxicalc/325718240901463" data-colorscheme="dark" data-numposts="20" data-width="600"></div>
 <br>
-<div class="fb-like-box" data-href="https://www.facebook.com/pages/Taxicalc/325718240901463" data-width="600" data-colorscheme="dark" data-show-faces="true" data-header="true" data-stream="true" data-show-border="true"></div>
-<br>
-
 
 <?php include("Epost.php");?>
 
